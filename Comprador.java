13544019052c0f6f569414605431474e6ed5ef36
Aremi <aremix.108@gmail.com>
2021-05-29 23:33:28 -0500

    Se arreglo el metodo CancelarPedido

--- Comprador.java
+++ Comprador.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Comprador extends Usuario {
-	private OrderListLinked<Carro> carrito;
+	
+	private ListLinked<Carro> carrito;
 	Scanner reader = new Scanner(System.in);
 	
 	public Comprador(String dni, String nombre, String apellidos, String email, String contraseña,
 		ArrayList<Integer> calificacion, String telefono) {
-	super(dni, nombre, apellidos, email, contraseña, calificacion, telefono);
-	// TODO Auto-generated constructor stub
+		
+		super(dni, nombre, apellidos, email, contraseña, calificacion, telefono);
+		// TODO Auto-generated constructor stub
 	}
 
 	public void RealizarPedido() {
@@ -23,12 +25,15 @@ public class Comprador extends Usuario {
 		destino = reader.nextLine();
 		c1.setDestino(destino);
 
-		System.out.print("Su codigo de su pedido es: "+c1.getCodigoPedido());
-		carrito.insertNode(c1);
+		System.out.print("Su codigo de su pedido es: "+c1.getCodigoVenta());
+		carrito.insertLast(c1);
+		System.out.println(" *** Gracias por su compra en Gricultor *** ");
 	}
+	
 	public void CancelarPedido() {
 		int cod;
-		System.out.print("¿Esta seguro de querer cancelar su pedido?(Si/No) ");
+		System.out.println("================= Cancelacion de Pedido =================");
+		System.out.print("\n¿Esta seguro de querer cancelar su pedido?(Si/No) ");
 		String res="";
 		Scanner reader = new Scanner(System.in);
 		res = reader.nextLine();
@@ -39,24 +44,28 @@ public class Comprador extends Usuario {
 		if(res=="Si"|| res== "si"|| res== "SI") {
 			System.out.print("Ingrese el codigo de su pedido: ");
 			cod=reader.nextInt();
-			if(carrito.search(cod)==-1) {
-				System.out.print("El numero de pedido ingresado no existe");
-				break;
-			}
-			else {
-				//de prueba
-				Carro c1;
-				carrito.remove(c1)=null;
-				System.out.println("Su pedido ha sido cancelado");
-			}
+			Node<Carro> auxNode=carrito.getFirst();
+			for(int i=0;i<carrito.length();i++) {
+				auxNode=carrito.getFirst();
+				if(auxNode.getData().getCodigoVenta()==cod) {
+					auxNode.getData().vaciarCarro();
+					System.out.println("Su pedido ha sido cancelado");
+				}
+			}	
+			System.out.print("El numero de pedido ingresado no existe");
+			
 		}
 		else{
-			break;
+			
+			System.out.println("Su pedido no se cancelara");
+			System.out.println(" *** Gracias por su compra *** ");
 		}
 	}
+	
 	@Override
 	public String toString() {
-		return "============ Comprador ============\\n"  
+		
+		return "\n============ Comprador ============\\n"  
 				+ "Nombre del comprador:\t" + getNombre()  
 				+" Apellidos:\t" + getApellidos()+"\nDNI:\t" + getDni()+ "\nEmail:\t" + getEmail() 
 				+ "\n Numero telefonico:\t"+ getTelefono()+ "\n Calificacion:\t" + getCalificacion();
